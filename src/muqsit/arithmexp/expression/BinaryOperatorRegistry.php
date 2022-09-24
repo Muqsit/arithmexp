aad629e87f32c999670a4662eca89a91cb7f82d3
Muqsit <hackhack05@gmail.com>
2022-09-24 08:09:01 +0000

    Use BinaryOperatorToken::OPERATOR_TYPE_* constants in BinaryOperatorRegistry.php

--- src/muqsit/arithmexp/expression/BinaryOperatorRegistry.php
+++ src/muqsit/arithmexp/expression/BinaryOperatorRegistry.php
@@ -6,15 +6,16 @@ namespace muqsit\arithmexp\expression;
 
 use Closure;
 use InvalidArgumentException;
+use muqsit\arithmexp\token\BinaryOperatorToken;
 
 final class BinaryOperatorRegistry{
 
 	public static function createDefault() : self{
 		$registry = new self();
-		$registry->register("/", static fn(int|float $x, int|float $y) : int|float => $x / $y);
-		$registry->register("*", static fn(int|float $x, int|float $y) : int|float => $x * $y);
-		$registry->register("+", static fn(int|float $x, int|float $y) : int|float => $x + $y);
-		$registry->register("-", static fn(int|float $x, int|float $y) : int|float => $x - $y);
+		$registry->register(BinaryOperatorToken::OPERATOR_TYPE_DIVISION, static fn(int|float $x, int|float $y) : int|float => $x / $y);
+		$registry->register(BinaryOperatorToken::OPERATOR_TYPE_MULTIPLICATION, static fn(int|float $x, int|float $y) : int|float => $x * $y);
+		$registry->register(BinaryOperatorToken::OPERATOR_TYPE_ADDITION, static fn(int|float $x, int|float $y) : int|float => $x + $y);
+		$registry->register(BinaryOperatorToken::OPERATOR_TYPE_SUBTRACTION, static fn(int|float $x, int|float $y) : int|float => $x - $y);
 		return $registry;
 	}
 
