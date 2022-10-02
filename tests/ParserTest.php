49a6fa535b436ff2f943a105cb1337b18c068d4b
Muqsit <hackhack05@gmail.com>
2022-10-02 08:18:19 +0000

    Compare `ParseException` attributes instead of exception message

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -8,6 +8,12 @@ use PHPUnit\Framework\TestCase;
 
 final class ParserTest extends TestCase{
 
+	private static function assertExceptionEquals(ParseException $exception, int $code, int $start_pos, int $end_pos) : void{
+		self::assertEquals($code, $exception->getCode());
+		self::assertEquals($start_pos, $exception->getStartPos());
+		self::assertEquals($end_pos, $exception->getEndPos());
+	}
+
 	private Parser $parser;
 
 	private function getParser() : Parser{
@@ -15,96 +21,126 @@ final class ParserTest extends TestCase{
 	}
 
 	public function testEmptyString() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Expression \"\" is empty");
-		$this->getParser()->parse("");
+		try{
+			$this->getParser()->parse("");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_EXPR_EMPTY, 0, 0);
+		}
 	}
 
 	public function testEmptyStringWithParentheses() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Expression \"( )\" is empty");
-		$this->getParser()->parse("( )");
+		try{
+			$this->getParser()->parse("( )");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_EXPR_EMPTY, 0, 3);
+		}
 	}
 
 	public function testSecludedNumericLiteral() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Unexpected Numeric Literal token encountered at \"3\" (6:7) in \"x + 2 3 ** y\"");
-		$this->getParser()->parse("x + 2 3 ** y");
+		try{
+			$this->getParser()->parse("x + 2 3 ** y");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_UNEXPECTED_TOKEN, 6, 7);
+		}
 	}
 
 	public function testSecludedFunctionCall() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Unexpected Function Call token encountered at \"tan(z)\" (16:22) in \"tan(x) + tan(y) tan(z) ** tan(w)\"");
-		$this->getParser()->parse("tan(x) + tan(y) tan(z) ** tan(w)");
+		try{
+			$this->getParser()->parse("tan(x) + tan(y) tan(z) ** tan(w)");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_UNEXPECTED_TOKEN, 16, 22);
+		}
 	}
 
 	public function testNoClosingParenthesis() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("No closing parenthesis specified for opening parenthesis at \"(\" (4:5) in \"x + (2 * y + (z / 2) + 5\"");
-		$this->getParser()->parse("x + (2 * y + (z / 2) + 5");
+		try{
+			$this->getParser()->parse("x + (2 * y + (z / 2) + 5");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_NO_CLOSING_PAREN, 4, 5);
+		}
 	}
 
 	public function testNoOpeningParenthesis() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("No opening parenthesis specified for closing parenthesis at \")\" (18:19) in \"x + (2 * y + z / 2)) + 5\"");
-		$this->getParser()->parse("x + (2 * y + z / 2)) + 5");
+		try{
+			$this->getParser()->parse("x + (2 * y + z / 2)) + 5");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_NO_OPENING_PAREN, 18, 19);
+		}
 	}
 
 	public function testNoUnaryOperand() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("No right operand specified for unary operator at \"+\" (2:3) in \"x*+\"");
-		$this->getParser()->parse("x*+");
+		try{
+			$this->getParser()->parse("x*+");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_NO_OPERAND_UNARY, 2, 3);
+		}
 	}
 
 	public function testNoBinaryLeftOperand() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("No left operand specified for binary operator at \"*\" (2:3) in \"()*2\"");
-		$this->getParser()->parse("()*2");
+		try{
+			$this->getParser()->parse("()*2");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_NO_OPERAND_BINARY_LEFT, 2, 3);
+		}
 	}
 
 	public function testNoBinaryRightOperand() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("No right operand specified for binary operator at \"*\" (1:2) in \"2*\"");
-		$this->getParser()->parse("2*");
+		try{
+			$this->getParser()->parse("2*");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_NO_OPERAND_BINARY_RIGHT, 1, 2);
+		}
 	}
 
 	public function testBadFunctionCallToUndefinedFunction() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Cannot resolve function call at \"noFunc()\" (4:12) in \"x * noFunc() / 3\": Function \"noFunc\" is not registered");
-		$this->getParser()->parse("x * noFunc() / 3");
+		try{
+			$this->getParser()->parse("x * noFunc() / 3");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_UNRESOLVABLE_FCALL, 4, 12);
+		}
 	}
 
 	public function testBadFunctionCallWithMalformedArgumentList() : void{
 		$this->getParser()->getFunctionRegistry()->register("malformedArgFnTest", static fn(int $x, int $y) : int => $x + $y);
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Unexpected Numeric Literal token encountered at \"3\" (25:26) in \"x + malformedArgFnTest(2 3) * y\"");
-		$this->getParser()->parse("x + malformedArgFnTest(2 3) * y");
+		try{
+			$this->getParser()->parse("x + malformedArgFnTest(2 3) * y");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_UNEXPECTED_TOKEN, 25, 26);
+		}
 	}
 
 	public function testBadFunctionCallWithMissingRequiredTrailingArguments() : void{
 		$this->getParser()->getFunctionRegistry()->register("missingTrailingArgFnTest", static fn(int $x, int $y) : int => $x + $y);
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Cannot resolve function call at \"missingTrailingArgFnTest(2, )\" (4:33) in \"x + missingTrailingArgFnTest(2, ) * y\": Function \"missingTrailingArgFnTest\" does not have a default value for parameter #2");
-		$this->getParser()->parse("x + missingTrailingArgFnTest(2, ) * y");
+		try{
+			$this->getParser()->parse("x + missingTrailingArgFnTest(2, ) * y");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_UNRESOLVABLE_FCALL, 4, 33);
+		}
 	}
 
 	public function testBadFunctionCallWithMissingRequiredLeadingArguments() : void{
 		$this->getParser()->getFunctionRegistry()->register("missingLeadingArgFnTest", static fn(int $x, int $y) : int => $x + $y);
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Cannot resolve function call at \"missingLeadingArgFnTest(, 2)\" (4:32) in \"x + missingLeadingArgFnTest(, 2) * y\": Function \"missingLeadingArgFnTest\" does not have a default value for parameter #1");
-		$this->getParser()->parse("x + missingLeadingArgFnTest(, 2) * y");
+		try{
+			$this->getParser()->parse("x + missingLeadingArgFnTest(, 2) * y");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_UNRESOLVABLE_FCALL, 4, 32);
+		}
 	}
 
 	public function testBadFunctionCallWithArgumentOverflow() : void{
 		$this->getParser()->getFunctionRegistry()->register("argOverflowFnTest", static fn(int $x, int $y) : int => $x + $y);
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Cannot resolve function call at \"argOverflowFnTest(3, 2, 1)\" (4:30) in \"x + argOverflowFnTest(3, 2, 1) * y\": Too many parameters supplied to function call: Expected 2 parameters, got 3 parameters");
-		$this->getParser()->parse("x + argOverflowFnTest(3, 2, 1) * y");
+		try{
+			$this->getParser()->parse("x + argOverflowFnTest(3, 2, 1) * y");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_UNRESOLVABLE_FCALL, 4, 30);
+		}
 	}
 
 	public function testArgumentSeparatorOutsideFunctionCall() : void{
-		$this->expectException(ParseException::class);
-		$this->expectExceptionMessage("Unexpected Function Call Argument Separator token encountered at \",\" (10:11) in \"2 + 3 * (4, 5) / 6\"");
-		$this->getParser()->parse("2 + 3 * (4, 5) / 6");
+		try{
+			$this->getParser()->parse("2 + 3 * (4, 5) / 6");
+		}catch(ParseException $e){
+			self::assertExceptionEquals($e, ParseException::ERR_UNEXPECTED_TOKEN, 10, 11);
+		}
 	}
 }
