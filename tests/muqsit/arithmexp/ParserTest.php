aca36bab9dd3622da529649b7b66b689567c6766
Muqsit <hackhack05@gmail.com>
2022-10-08 08:32:18 +0000

    Move ParserTest::assertParserThrows() to TestUtil

--- tests/muqsit/arithmexp/ParserTest.php
+++ tests/muqsit/arithmexp/ParserTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace muqsit\arithmexp;
 
 use PHPUnit\Framework\TestCase;
-use RuntimeException;
 
 final class ParserTest extends TestCase{
 
@@ -15,84 +14,71 @@ final class ParserTest extends TestCase{
 		$this->parser = Parser::createDefault();
 	}
 
-	private function assertParserThrows(string $expression, int $code, int $start_pos, int $end_pos) : void{
-		try{
-			$this->parser->parse($expression);
-		}catch(ParseException $e){
-			self::assertEquals($code, $e->getCode());
-			self::assertEquals($start_pos, $e->getStartPos());
-			self::assertEquals($end_pos, $e->getEndPos());
-			return;
-		}
-
-		throw new RuntimeException("Expression \"{$expression}\" did not throw any exception (expected " . ParseException::class . "(code: {$code}, start_pos: {$start_pos}, end_pos: {$end_pos}))");
-	}
-
 	public function testEmptyString() : void{
-		$this->assertParserThrows("", ParseException::ERR_EXPR_EMPTY, 0, 0);
+		TestUtil::assertParserThrows($this->parser, "", ParseException::ERR_EXPR_EMPTY, 0, 0);
 	}
 
 	public function testEmptyStringWithParentheses() : void{
-		$this->assertParserThrows("( )", ParseException::ERR_EXPR_EMPTY, 0, 3);
+		TestUtil::assertParserThrows($this->parser, "( )", ParseException::ERR_EXPR_EMPTY, 0, 3);
 	}
 
 	public function testSecludedNumericLiteral() : void{
-		$this->assertParserThrows("x + 2 3 ** y", ParseException::ERR_UNEXPECTED_TOKEN, 6, 7);
+		TestUtil::assertParserThrows($this->parser, "x + 2 3 ** y", ParseException::ERR_UNEXPECTED_TOKEN, 6, 7);
 	}
 
 	public function testSecludedFunctionCall() : void{
-		$this->assertParserThrows("tan(x) + tan(y) tan(z) ** tan(w)", ParseException::ERR_UNEXPECTED_TOKEN, 16, 22);
+		TestUtil::assertParserThrows($this->parser, "tan(x) + tan(y) tan(z) ** tan(w)", ParseException::ERR_UNEXPECTED_TOKEN, 16, 22);
 	}
 
 	public function testNoClosingParenthesis() : void{
-		$this->assertParserThrows("x + (2 * y + (z / 2) + 5", ParseException::ERR_NO_CLOSING_PAREN, 4, 5);
+		TestUtil::assertParserThrows($this->parser, "x + (2 * y + (z / 2) + 5", ParseException::ERR_NO_CLOSING_PAREN, 4, 5);
 	}
 
 	public function testNoOpeningParenthesis() : void{
-		$this->assertParserThrows("x + (2 * y + z / 2)) + 5", ParseException::ERR_NO_OPENING_PAREN, 18, 19);
+		TestUtil::assertParserThrows($this->parser, "x + (2 * y + z / 2)) + 5", ParseException::ERR_NO_OPENING_PAREN, 18, 19);
 	}
 
 	public function testNoUnaryOperand() : void{
-		$this->assertParserThrows("x*+", ParseException::ERR_NO_OPERAND_UNARY, 2, 3);
+		TestUtil::assertParserThrows($this->parser, "x*+", ParseException::ERR_NO_OPERAND_UNARY, 2, 3);
 	}
 
 	public function testNoBinaryLeftOperand() : void{
-		$this->assertParserThrows("()*2", ParseException::ERR_NO_OPERAND_BINARY_LEFT, 2, 3);
+		TestUtil::assertParserThrows($this->parser, "()*2", ParseException::ERR_NO_OPERAND_BINARY_LEFT, 2, 3);
 	}
 
 	public function testNoBinaryRightOperand() : void{
-		$this->assertParserThrows("2*", ParseException::ERR_NO_OPERAND_BINARY_RIGHT, 1, 2);
+		TestUtil::assertParserThrows($this->parser, "2*", ParseException::ERR_NO_OPERAND_BINARY_RIGHT, 1, 2);
 	}
 
 	public function testBadFunctionCallToUndefinedFunction() : void{
-		$this->assertParserThrows("x * noFunc() / 3", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 12);
+		TestUtil::assertParserThrows($this->parser, "x * noFunc() / 3", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 12);
 	}
 
 	public function testBadFunctionCallWithMalformedArgumentList() : void{
 		$this->parser->getFunctionRegistry()->register("malformedArgFnTest", static fn(int $x, int $y) : int => $x + $y);
-		$this->assertParserThrows("x + malformedArgFnTest(2 3) * y", ParseException::ERR_UNEXPECTED_TOKEN, 25, 26);
+		TestUtil::assertParserThrows($this->parser, "x + malformedArgFnTest(2 3) * y", ParseException::ERR_UNEXPECTED_TOKEN, 25, 26);
 	}
 
 	public function testBadFunctionCallWithMissingRequiredTrailingArguments() : void{
 		$this->parser->getFunctionRegistry()->register("missingTrailingArgFnTest", static fn(int $x, int $y) : int => $x + $y);
-		$this->assertParserThrows("x + missingTrailingArgFnTest(2, ) * y", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 33);
+		TestUtil::assertParserThrows($this->parser, "x + missingTrailingArgFnTest(2, ) * y", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 33);
 	}
 
 	public function testBadFunctionCallWithMissingRequiredLeadingArguments() : void{
 		$this->parser->getFunctionRegistry()->register("missingLeadingArgFnTest", static fn(int $x, int $y) : int => $x + $y);
-		$this->assertParserThrows("x + missingLeadingArgFnTest(, 2) * y", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 32);
+		TestUtil::assertParserThrows($this->parser, "x + missingLeadingArgFnTest(, 2) * y", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 32);
 	}
 
 	public function testBadFunctionCallWithArgumentUnderflow() : void{
-		$this->assertParserThrows("x + fdiv(1) * y", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 11);
+		TestUtil::assertParserThrows($this->parser, "x + fdiv(1) * y", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 11);
 	}
 
 	public function testBadFunctionCallWithArgumentOverflow() : void{
 		$this->parser->getFunctionRegistry()->register("argOverflowFnTest", static fn(int $x, int $y) : int => $x + $y);
-		$this->assertParserThrows("x + argOverflowFnTest(3, 2, 1) * y", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 30);
+		TestUtil::assertParserThrows($this->parser, "x + argOverflowFnTest(3, 2, 1) * y", ParseException::ERR_UNRESOLVABLE_FCALL, 4, 30);
 	}
 
 	public function testArgumentSeparatorOutsideFunctionCall() : void{
-		$this->assertParserThrows("2 + 3 * (4, 5) / 6", ParseException::ERR_UNEXPECTED_TOKEN, 10, 11);
+		TestUtil::assertParserThrows($this->parser, "2 + 3 * (4, 5) / 6", ParseException::ERR_UNEXPECTED_TOKEN, 10, 11);
 	}
 }
---
+++ tests/muqsit/arithmexp/TestUtil.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace muqsit\arithmexp;
+
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+final class TestUtil{
+
+	public static function assertParserThrows(Parser $parser, string $expression, int $code, int $start_pos, int $end_pos) : void{
+		try{
+			$parser->parse($expression);
+		}catch(ParseException $e){
+			TestCase::assertEquals($code, $e->getCode());
+			TestCase::assertEquals($start_pos, $e->getStartPos());
+			TestCase::assertEquals($end_pos, $e->getEndPos());
+			return;
+		}
+
+		throw new RuntimeException("Expression \"{$expression}\" did not throw any exception (expected " . ParseException::class . "(code: {$code}, start_pos: {$start_pos}, end_pos: {$end_pos}))");
+	}
+}
