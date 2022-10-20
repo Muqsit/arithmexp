16ff6be7abcd7d41045df908a69ea798ed8dfacf
Muqsit <hackhack05@gmail.com>
2022-10-20 04:58:00 +0000

    Fix incorrect unexpected parenthesis token pos in ParseException

--- src/muqsit/arithmexp/Parser.php
+++ src/muqsit/arithmexp/Parser.php
@@ -186,7 +186,7 @@ final class Parser{
 					continue;
 				}
 				if($member_token->getParenthesisType() !== $token->getParenthesisType()){
-					throw ParseException::unexpectedParenthesisType($expression, $token->getPos());
+					throw ParseException::unexpectedParenthesisType($expression, $member_token->getPos());
 				}
 				if($member_token->getParenthesisMark() !== ParenthesisToken::MARK_CLOSING){
 					throw ParseException::noClosingParenthesis($expression, $token->getPos());
